dde8f467e103e5f785dbe997a568b06d41489054
Alfian Fathur Rohman <rohmanfatur.alfian@gmail.com>
2025-07-19 20:29:39 +0700

    add icon (splash and launcher)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:laporin/firebase_options.dart';
 import 'package:laporin/routes/app_router.dart';
@@ -11,12 +12,13 @@ import 'package:laporin/providers/user_provider.dart'; // AsyncNotifierProvider
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // 🔔 Inisialisasi notifikasi
   await NotificationService.init();
   await initializeDateFormatting('id_ID', null);
 
@@ -30,12 +32,14 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final userState = ref.watch(userProvider);
 
-    // Router akan dibuat berdasarkan status login user
     final router = userState.when(
-      data: (user) => AppRouter.createRouter(user), // kamu harus modifikasi createRouter agar menerima UserState
+      data: (user) => AppRouter.createRouter(user),
       loading: () => AppRouter.createRouter(null),
       error: (e, _) => AppRouter.createRouter(null),
     );
+
+    /// ❗️Pindahkan di sini agar splash hilang setelah login state diketahui
+    FlutterNativeSplash.remove();
 
     return MaterialApp.router(
       title: 'Lapor.in',
